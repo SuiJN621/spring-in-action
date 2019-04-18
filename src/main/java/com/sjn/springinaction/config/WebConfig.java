b277a88ce5f7ad4d761d0556ad02aa184f303154
SuiJN <SuiJN93@outlook.com>
2019-04-18 14:51:03 +0800

    update ch4

--- src/main/java/com/sjn/springinaction/config/WebConfig.java
+++ src/main/java/com/sjn/springinaction/config/WebConfig.java
@@ -22,5 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/user/login").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
     }
 }
