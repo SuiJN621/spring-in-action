1617c0f5d927d67b3fc1eef41058b8d848c31023
SuiJN <SuiJN93@outlook.com>
2019-04-18 14:50:35 +0800

    update ch4

--- src/main/java/com/sjn/springinaction/controller/TacoOrderController.java
+++ src/main/java/com/sjn/springinaction/controller/TacoOrderController.java
@@ -3,15 +3,18 @@ package com.sjn.springinaction.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import com.sjn.springinaction.entity.User;
 import com.sjn.springinaction.repository.jpa.JpaOrderRepository;
 import com.sjn.springinaction.entity.Order;
 import com.sjn.springinaction.repository.jdbc.OrderRepository;
@@ -37,8 +40,25 @@ public class TacoOrderController {
     }
 
     @GetMapping("/current")
-    public String current(Model model){
+    public String current(@AuthenticationPrincipal User user,
+                          @ModelAttribute Order order){
         //model.addAttribute("order", new Order());
+        //获取用户设置回显数据
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
         return "orderForm";
     }
 
@@ -47,11 +67,16 @@ public class TacoOrderController {
                                //Validation错误对象, 也可以使用BindingResult
                                Errors errors,
                                //获取session状态
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               //注解等效于Authentication.getPrincipal()
+                               //更通用的获取用户的方式
+                               //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        order.setUser(user);
         log.info("Order submitted: " + order);
         //orderRepo.save(order);
         jpaOrderRepository.save(order);
---
+++ src/main/java/com/sjn/springinaction/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.sjn.springinaction.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sjn.springinaction.formbean.RegistrationForm;
+import com.sjn.springinaction.repository.jpa.UserRepository;
+
+/**
+ * @author Sui
+ * @date 2019.04.18 9:58
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public UserController(
+            UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String register(RegistrationForm form) {
+        userRepository.save(form.toUser(passwordEncoder));
+        return "redirect:/user/login";
+    }
+}
